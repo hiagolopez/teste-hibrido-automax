2584a7302fa30b588078e71758a2d592608712cc
Hiago Lopez <hiago.lopez@ciahering.com.br>
2025-12-09 02:32:39 -0300

    feat: adicionado footer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import Header from './react/components/Header';
+import Footer from './react/components/Footer';
 import Home from './pages/Home';
 import StoreLocator from './react/StoreLocator';
+import styles from './App.module.scss';
 
 function App() {
   return (
     <Router>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/nossas-lojas" component={StoreLocator} />
-        <Redirect to="/" />
-      </Switch>
+      <div className={styles.appContainer}>
+        <Header />
+        <main className={styles.mainContent}>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/nossas-lojas" component={StoreLocator} />
+            <Redirect to="/" />
+          </Switch>
+        </main>
+        <Footer />
+      </div>
     </Router>
   );
 }
